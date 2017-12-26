nitin1438/android_device_moto_shamu

Build dumpstate HAL
Change-Id: I9b2ddf932270adbccfb30cdb65abd628f4a2b23d

device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
+# Dumpstate HAL
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.shamu
+
 # For SPN display
 PRODUCT_COPY_FILES += \
     device/moto/shamu/spn-conf.xml:system/etc/spn-conf.xml

hammy007/android_device_lge_hammerhead

Change-Id: If20b988e033a6e0d2ac0bdd9bb788d881a1bd3eb

device.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libqdMetaData
 
+# Dumpstate HAL
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.hammerhead
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
@@ -471,3 +475,4 @@ ro.du.updater=hammerhead
 $(call inherit-product-if-exists, hardware/qcom/msm8x74/msm8x74.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x74/msm8x74-gpu-vendor.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
+