616b5b653793f8347e61265eb54e769e90c53209
D. Andrei Măceș <dmaces@nd.edu>
2018-01-06 18:17:09 +0200

    audio: Migrate to XML audio policy configuration
    Partially inspired by:
    https://android.googlesource.com/device/lge/bullhead/+/b1d6440e7
    
    While at it, use TARGET_COPY_OUT_VENDOR with all changed paths.
    
    Change-Id: I6a0ef23b8a366da66a99713a9bf4c180448e18e1
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- device.mk
+++ device.mk
@@ -169,6 +169,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler
+# Audio HAL and utilities
+USE_XML_AUDIO_POLICY_CONF := 1
 
 # Audio effects
 PRODUCT_PACKAGES += \
@@ -179,6 +181,11 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     device/lge/hammerhead/audio_policy.conf:system/etc/audio_policy.conf \
     device/lge/hammerhead/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     device/lge/hammerhead/mixer_paths.xml:system/etc/mixer_paths.xml \
